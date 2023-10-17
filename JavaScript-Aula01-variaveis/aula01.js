f3abbf88e089f1b2ffb353ed88dfc4293122d2f2
Anderson Pereira Frazão <141861827+frazao090@users.noreply.github.com>
2023-10-16 21:09:56 -0300

    update

---
+++ JavaScript-Aula01-variaveis/aula01.js
@@ -0,0 +1,130 @@
+// const num = 42;
+// console.log(typeof num);
+
+// console.log("Estou no HTML fazendo testes")
+
+// let numero = 55;
+// console.log(typeof numero);
+
+// const number = 3.14;
+// const number2 = 5
+// const hello = "Olá, mundo!";
+// console.log(number, hello,number2);
+
+// let palavra = 'Vamos estudar!'
+// console.log(typeof palavra);
+
+// let vouEstudar = true;
+// let naoVouEstudar = false;
+
+// console.log(typeof vouEstudar);
+
+// let nulo = null;
+// console.log(nulo);
+
+// let valor
+// console.log(valor);
+
+// var soma = 1 + 10 + 9
+// soma = 25
+// soma = 20
+// soma = 5 * (2 - 9)
+
+// // console.log(soma);
+
+// let cpf1 = 379825930-54
+
+// let cpf2 = "379825930-54"
+
+// console.log(cpf1);
+
+// let num1 = 5;
+// let num2 = 10;
+
+// let soma = num1 + num2;
+
+// let pi = 3.14;
+// let raio = 5;
+
+// let comprimentoDaCircunferencia = (raio*pi)*2;
+
+// console.log(comprimentoDaCircunferencia);
+
+// let temperaturaCelsius = 30;
+// let fahrenheit = ((temperaturaCelsius*9)/5)+32;
+// console.log(fahrenheit);
+
+// let nota1 = 7;
+// let nota2 = 9;
+// let media = (nota1+nota2)/2;
+
+// console.log(media);
+
+// let largura = 10;
+// let comprimento = 20;
+// let area = largura*comprimento;
+// console.log(area);
+
+// let salarioBruto = 5000;
+// let salarioLiquido = salarioBruto - (salarioBruto*0.1)
+// console.log(salarioLiquido);
+
+
+// let horasTrabalhadas = 40;
+// let valorHora = 50;
+// let salarioBrutro = horasTrabalhadas*valorHora;
+
+// console.log(`O salário do funcionário é de R$ ${salarioBrutro}`);
+
+// Transformando String em Number:
+
+// const numero1 = "10";
+// const numero2 = "20.5";
+
+// console.log(typeof numero1); // string
+// console.log(typeof numero2); // string
+
+// // Usando parseInt
+// const resultado1 = parseInt(numero1);
+// console.log(resultado1); // Output: 10
+// console.log(typeof resultado1); // number
+
+// // Usando parseFloat
+// const resultado2 = parseFloat(numero2);
+// console.log(resultado2); // Output: 20.5
+// console.log(typeof resultado2); // number
+
+// let nome = "João";
+// let sobrenome = "Silva"
+// console.log(`Olá ${nome} ${sobrenome}, como posso ajudá-lo?`);
+
+// let string1 = "Hello";
+// let string2 = "World";
+// let string3 = string1 + string2;
+// console.log(string3);
+
+// let numero = "12";
+// let numero2 = "3"
+// let numeroInt = parseInt(numero);
+// let numeroInt2 = parseInt(numero2);
+
+// let soma = numeroInt + numeroInt2;
+// console.log(soma);
+
+
+// console.log("Kenzie Academy"[7]);
+
+// let minhaString = "Caixa d\’água"
+
+// console.log(minhaString)
+
+// console.log("Dev Full-stack".length);
+
+// const a = 4
+
+// const b = 3
+
+// a = 2
+
+// console.log(a, b);
+
