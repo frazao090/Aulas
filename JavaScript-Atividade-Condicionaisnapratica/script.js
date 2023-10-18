c9057a6fa780d9188ae2c1bb53398979e05de3cf
Anderson Pereira Frazão <141861827+frazao090@users.noreply.github.com>
2023-10-18 11:02:13 -0300

    up

---
+++ JavaScript-Atividade-Condicionaisnapratica/script.js
@@ -0,0 +1,75 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if(a+b < c){
+// console.log("Sim, a soma de a + b é maior q c" );
+
+// }else if(a+b == c){
+//     console.log("A soma de a + b é igual a c")
+// }else{
+//     console.log("a + b é menor que c")
+// }
+
+// let numero = prompt("Digite um número: ")
+
+// if(numero%2 == 0){
+//     alert("O número é par");
+// }else{
+//     alert("O número é impar");
+// }
+
+// let euGostoDeBeber = true;
+// let euNaoBebo = false;
+
+// console.log(euGostoDeBeber && euNaoBebo || euGostoDeBeber);
+
+// let a = 100;
+//  let b = 90;
+//  let c = 99;
+
+// if( a>b && a>c && b>c){
+//     console.log(a,b,c);
+// }else if( a>b && a>c && c>b){
+//     console.log(a,c,b)
+// }else if( b>a && b>c && a>c){
+//     console.log(b,a,c);
+// }else if( b>a && b>c && c>a){
+//     console.log(b,c,a)
+// }else if( c>a && c>b && b>a){
+//     console.log(c,b,a);
+// }else if( c>a && c>b && a>b){
+//     console.log(c,a,b)
+// }else{
+//     console.log("Você digitou um número que não é inteiro")
+// }
+
+// let numero1 = prompt("Digite o primeiro número");
+
+// let numero2 = prompt("Digite o segundo número");
+
+// if(numero1 > numero2){
+//     alert(`O número ${numero1} é maior`);
+
+// }else if(numero1 == numero2){
+//     alert("Os números são iguais")
+// }else{
+//     alert(`O número ${numero2} é maior` )
+// }
+
+// let carro = "Cobalt"
+
+// if (carro.length === 5 && carro === "Cobalt") {
+//   console.log(`O tamanho da variável é igual a ${carro.length} e seu valor é ${carro}.`)
+// } else if (carro.length === 6 || carro === "Ferrari") {
+//   console.log(`O tamanho da variável é igual a ${carro.length} e seu valor é ${carro}.`)
+// } else {
+//   console.log(`Carro não identificado.`)
+// }
+
+// let idade = 20
+// if (idade == "20") {
+//   console.log("Sim")
+// } else {
+//   console.log("Não")
+// }
---
+++ javaScript-aula02-promptealert/script.js
@@ -0,0 +1,30 @@
+// let nome = prompt("Qual o seu nome?");
+// alert("Seja bem vindo(a), " + nome + '!' );
+
+// alert("Oi! Bem-vindos à Kenzie Academy Brasil!")
+
+// let numCorreto = 7;
+// let palpite = prompt("Advinhe o número que estou pensando, de 1 a 10!");
+
+// if(palpite == numCorreto){
+// alert("Parabéns!! você acertou!")
+// }else{
+// alert('Você errou!')  
+
+// }
+
+// const a = 10;
+// const b = 5;
+
+// console.log(a < b);
+
+// console.log(1 !== "1");
+
+// let dinheiroJose = 12;
+// let precoOvos = 10.20;
+
+// if(dinheiroJose >= precoOvos){
+//     console.log("José comprou os ovos")
+// }else {
+//     console.log("José voltou sem os ovos")
+// }
